ff514c7e33a523b39679a785d3d44064f1f24f4a
makandamakanda776-design <makandamakanda776@gmail.com>
2026-01-06 22:16:59 +0200

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,9 +1,9 @@
-//=====🏅 ᴍᴀʟᴠɪɴ  xᴅ🔥====
+//=====🏅 Silent nodie xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'ᴍᴀʟᴠɪɴ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'SILENT xᴅ', // ur bot name
+  OWNER_NAME: 'SILENT ᴋɪɴɢ', //your name
+  OWNER_NUMBER: '263784549656', // add your phone number
   SESSION_ID: '', // add your session ID
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare' // ur timezone
